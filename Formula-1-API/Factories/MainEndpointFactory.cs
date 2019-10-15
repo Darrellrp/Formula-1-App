b7cfaa9af6d00e2cec7f1f855c4f5987a4e01898
D.R. Poleon <darrell.poleon@hva.nl>
2019-10-16 01:19:42 +0200

    Minor change

--- Formula-1-API/Factories/MainEndpointFactory.cs
+++ Formula-1-API/Factories/MainEndpointFactory.cs
@@ -9,7 +9,7 @@ namespace Formula_1_API.Factories
     public static class MainEndpointFactory
     {
         private static readonly string Name = "Formula 1 REST API - Main endpoint";
-        private static readonly string Description = "This api contains data from 1950 all the way through the 2017 season, and consists of tables describing constructors, race drivers, lap times, pit stops and more.";
+        private static readonly string Description = "This API contains data from 1950 all the way through the 2017 season, and consists of tables describing constructors, race drivers, lap times, pit stops and more.";
         private static readonly string Version = "1.0";
         private static readonly string Source = "https://www.kaggle.com/cjgdev/formula-1-race-data-19502017";
 
