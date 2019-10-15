e13275f4b6e73e19cc7d7f1e85cbc43c821b8035
D.R. Poleon <darrell.poleon@hva.nl>
2019-10-15 14:24:01 +0200

    Minor view changes + Added CsvReaderFactory

---
+++ Formula-1-API/Factories/CsvReaderFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Formula_1_API.Utils;
+
+namespace Formula_1_API.Factories
+{
+    public static class CsvReaderFactory
+    {
+        private const string BasePath = "Data/formula-1-race-data";
+
+        public static CsvReader Create(string basePath = BasePath)
+        {
+            return new CsvReader(basePath);
+        }
+    }
+}
--- Formula-1-API/Seeders/EFDatabaseSeeder.cs
+++ Formula-1-API/Seeders/EFDatabaseSeeder.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using CsvHelper.Configuration;
+using Formula_1_API.Factories;
 using Formula_1_API.Models;
 using Formula_1_API.Utils;
 using Formula_1_API.Utils.ClassMaps;
@@ -15,7 +16,7 @@ namespace Formula_1_API.Seeders
 
         public static ModelBuilder Seed<T>(string filename, ModelBuilder modelBuilder, int? limit = null, bool setIds = false) where T : class, IIdentifier
         {
-            var reader = new CsvReader(BasePath);
+            var reader = CsvReaderFactory.Create(BasePath);
             var items = reader.Read<T>(filename).ToList();
 
             if(limit != null)
@@ -35,7 +36,7 @@ namespace Formula_1_API.Seeders
 
         public static ModelBuilder Seed<T, Map>(string filename, ModelBuilder modelBuilder, int? limit = null, bool setIds = false) where T : class, IIdentifier where Map : ClassMap
         {
-            var reader = new CsvReader(BasePath);
+            var reader = CsvReaderFactory.Create(BasePath);
             var items = reader.Read<T, Map>(filename).ToList();
 
             if (limit != null)
