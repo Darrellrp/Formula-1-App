1376bc5c81bd73ed940de1c29c4138e93783346a
D. Poleon <darrellp@hotmail.nl>
2022-10-20 22:14:04 +0200

    Added Index.cshtml

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Formula_1_App.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
