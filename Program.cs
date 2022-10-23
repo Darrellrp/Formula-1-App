d232c90c77d8dc646a9ead8f2a9bc720126f7fd2
D.R. Poleon <Darrellp@hotmail.nl>
2022-10-23 15:18:11 +0200

    Added project changes

--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
 using Formula_1_App.Configurations;
+using Formula_1_App.Context;
+using Formula_1_App.Controllers;
+using Formula_1_App.Factories;
+using Formula_1_App.Factories.Interfaces;
+using Formula_1_App.Hubs;
+using Formula_1_App.Models;
+using Formula_1_App.Repositories;
+using Formula_1_App.Repositories.Adapters;
+using Formula_1_App.Repositories.Interfaces;
 using Formula_1_App.Seeders;
+using Formula_1_App.Services;
+using Formula_1_App.Services.Interfaces;
+using Formula_1_App.Subjects;
+using Formula_1_App.Subjects.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
-if (args.Length != 0 && (args[0].Equals("-s") || args[0].Equals("--seed")))
+var builder = WebApplication.CreateBuilder(args);
+
+if(builder.Environment.IsProduction()) {
+    DotNetEnv.Env.Load(".env");
+} else
 {
-    Console.WriteLine("Seeding Database...");
-    EFDatabaseSeeder.SeedAll();
-    Environment.Exit(1);
+    DotNetEnv.Env.Load("development.env");
 }
 
-var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient(typeof(DbContextOptions<Formula1DbContext>), typeof(DbContextOptions<Formula1DbContext>));
+builder.Services.AddTransient(typeof(IDatasourceAdapter<>), typeof(EntityFrameworkAdapter<>));
+builder.Services.AddTransient(typeof(IRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped(typeof(IService<>), typeof(BaseService<>));
+builder.Services.AddScoped(typeof(ISubject<>), typeof(BaseSubject<>));
+builder.Services.AddScoped(typeof(BaseController<>), typeof(BaseController<>));
+builder.Services.AddTransient(typeof(EntityFrameworkAdapter<>), typeof(EntityFrameworkAdapter<>));
+builder.Services.AddTransient(typeof(MongoAdapter<>), typeof(MongoAdapter<>));
+builder.Services.AddScoped(typeof(MainEndpointFactory), typeof(MainEndpointFactory));
+builder.Services.AddScoped(typeof(EndpointFactory), typeof(EndpointFactory));
+
+builder.Services.AddTransient<DbContext, Formula1DbContext>();
+
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTIONSTRING");
+
+builder.Services.AddDbContext<Formula1DbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Add services to the container.
 builder.Services.Configure<ApiConfiguration>(builder.Configuration.GetSection("ApiConfiguration"));
@@ -17,6 +48,8 @@ builder.Services.Configure<ApiConfiguration>(builder.Configuration.GetSection("A
 builder.Services.AddControllers();
 
 builder.Services.AddRazorPages();
+
+builder.Services.AddSignalR();
 
 builder.Services.AddCors(options =>
 {
@@ -28,6 +61,24 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+if (args.Length != 0 && (args[0].Equals("-s") || args[0].Equals("--seed")))
+{
+    Console.WriteLine("Seeding Database...");
+
+    var seeder = new EFDatabaseSeeder(app.Services);
+
+    try
+    {        
+        await seeder.SeedAll(limit: 100);
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"An error occured during database seeding: {exception}");
+    }
+
+    Environment.Exit(1);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -56,6 +107,8 @@ app.UseCors();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapHub<EntityHub>("/signalr");
 
 app.MapRazorPages();
 
