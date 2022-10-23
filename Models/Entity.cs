89198dd19c15d04fbd24150647ee65004f15cfd8
D.R. Poleon <Darrellp@hotmail.nl>
2022-10-23 15:07:36 +0200

    Added Entity & Response model

---
+++ Models/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Formula_1_App.Models
+{
+    public abstract class Entity: IEntity
+    {
+        public abstract int? Id { get; set; }
+
+        public Entity() { }
+
+        public override bool Equals(object obj) => this.Equals((Entity)obj);
+
+        public bool Equals(Entity entity)
+        {
+            if (entity == null)
+            {
+                return false;
+            }
+
+            return entity.Id.Equals(Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
+
