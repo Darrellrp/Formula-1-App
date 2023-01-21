94a748565215579162567ca6ab95a430556ae051
D.R. Poleon <Darrellp@hotmail.nl>
2023-01-21 02:07:04 +0100

    Remove old FE

--- Formula-1-Web/src/app/app.module.ts
+++ Formula-1-Web/src/app/app.module.ts
@@ -6,31 +6,22 @@ import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
-import { HomeComponent } from './components/home/home.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
-import { ConfigurationService } from './services/configuration.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      // { path: '', component: HomeComponent, pathMatch: 'full' },
+      // { path: 'counter', component: CounterComponent },
+      // { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [ConfigurationService],
+  // providers: [ConfigurationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
