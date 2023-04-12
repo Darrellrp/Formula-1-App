bf5c1c4661e7bbbc4c29d13a9d1ea2378cc259ea
D.R. Poleon <Darrellp@hotmail.nl>
2023-04-12 20:33:56 +0200

    Revert "Removed unnecessary .NET pages"
    This reverts commit 4ab4c13495966a943a601db38b148b334dd9c7e6.

---
+++ Formula-1-API/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Formula_1_API.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string ApiServerUrl { get; set; } = string.Empty;
+
+        public void OnGet()
+        {
+            this.ApiServerUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}/api";
+        }
+    }
+}
